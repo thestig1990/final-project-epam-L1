d6457c3d6246fc388159bd9986ad6b38feaade7a
thestig1990 <yakymov1geka@gmail.com>
2023-02-11 17:25:53 +0200

    added new file

---
+++ terraform/scripts/display-elb-dns.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+
+DOMAIN=$(aws elb describe-load-balancers --query "LoadBalancerDescriptions[?LoadBalancerName == '${UNIQUE_IDENTIFIER}-webserver'].DNSName" --output text)
+
+
+echo "Application successfully deployed! Please visit http://$DOMAIN in your browser to view it."
