6bc7303961223995cc3fb7fe2badc553dbcae2a3
thestig1990 <yakymov1geka@gmail.com>
2023-02-26 12:53:48 +0200

    update terraform file

--- terraform/compute/iam.tf
+++ terraform/compute/iam.tf
@@ -1,10 +1,14 @@
 # Creating an IAM role
+# "assume_role_policy" - the policy that grants an entity permission to assume the role.
+# (allows EC2 instances to call AWS services on your behalf)
 resource "aws_iam_role" "deploy_role" {
   name               = "${var.UNIQUE_IDENTIFIER}-deploy-role"
-  assume_role_policy = file("compute/policies/assume_role_policy.json") # The policy that grants an entity permission to assume the role.
+  assume_role_policy = file("compute/policies/assume_role_policy.json")
 }
 
 # Creating an IAM instance profile
+# The instance profile contains the role and can provide the role's 
+# temporary credentials to an application that runs on the instance.
 resource "aws_iam_instance_profile" "deploy_profile" {
   name = "${var.UNIQUE_IDENTIFIER}-deploy-profile"
   role = aws_iam_role.deploy_role.name
